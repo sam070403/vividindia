fde89ae24b1320b864f1115c2f78ff48f25033ee
Sam <adhausamyak@gmail.com>
2024-08-13 14:15:19 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,7 +45,7 @@ export const signin=async(req,res,next)=>{
             process.env.JWT_SECRET
         );
 
-        const{password:pass,...rest}=validUser._doc;
+        const{password:pass,...rest}= validUser._doc;
         res.status(200).cookie('access_token',token,{httpOnly:true}).json(rest);
 
      } catch (error) {
